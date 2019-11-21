accb234c21b362cab277a9a8fb69450b4785560d
Danny Gleason <danny.c.gleason@gmail.com>
2019-11-21 15:26:34 -0500

    added a few changes

--- main.js
+++ main.js
@@ -3,7 +3,21 @@
 //         $(this).toggleClass('open');
 //         $('.top-nav').toggleClass('open');
 //     });
+
+//     $('nav a[href*="#]').on('click', function(){
+//         $('html, body').animate(keyframes:{
+//             scrollTop: 0
+//         } options 2000);
+//         })
+//     }       
+//     $('#up').on('click', function(){
+//         $('html, body').animate(keyframes:{
+//             scrollTop: 0
+//         }, options 2000);
+//         });
+   
 // });
+
 
 var theToggle = document.getElementById('toggle');
 
@@ -47,3 +61,5 @@ theToggle.onclick = function() {
    toggleClass(this, 'on');
    return false;
 }
+
+
