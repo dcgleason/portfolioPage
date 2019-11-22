6c30d42e3ee15ace90be1d61f64d1221bdc20863
Danny Gleason <danny.c.gleason@gmail.com>
2019-11-21 23:04:30 -0500

    added button functionality

--- main.js
+++ main.js
@@ -63,3 +63,8 @@ theToggle.onclick = function() {
 }
 
 
+$('#up').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
